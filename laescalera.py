1764f402ef82698b061363bbf2a9eafcf5bc47fd
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2021-12-14 18:57:57 +0100

    escalera hecha

--- laescalera.py
+++ laescalera.py
@@ -9,8 +9,14 @@ import sys
 # The function accepts INTEGER n as parameter.
 #
 def escalera(n):
-# Write your code here
+    for i in range(0, n):
+        linea = ""
+        for k in range(0,n-1-i):
+            linea = linea + " "
+        for j in range(0,i + 1):
+            linea = linea + "€"
+        print(linea)
 
 if __name__ == '__main__':
-n = int(input().strip())
+   n = int(input("Introduce las dimensiones que desees de la escalera:").strip())
 escalera(n)
