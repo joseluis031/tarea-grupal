6a1825a10af7188cc18d6c038b9abdf4b83543ac
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2021-12-14 17:57:34 +0100

    genero las notas aleatorias y las printeo

---
+++ comparalosproblemas.py
@@ -0,0 +1,12 @@
+from random import randint
+
+nota1 = randint(0,10)
+nota2 = randint(0,10)
+nota3 = randint(0,10)
+
+nota4 = randint(0,10)
+nota5 = randint(0,10)
+nota6 = randint(0,10)
+
+print("Las notas de Lucía son:\nClaridad del problema = {} \nOriginalidad = {} \nDificultad = {}".format(nota1,nota2,nota3))
+print("Las notas de Carlos son:\nClaridad del problema = {} \nOriginalidad = {} \nDificultad = {}".format(nota4,nota5,nota6))
