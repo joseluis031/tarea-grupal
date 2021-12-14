4d130c1b2082732697d8f0770c5dc243c56ce8d3
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2021-12-14 18:05:02 +0100

    comparo la nota de cada criterio de cada uno

--- comparalosproblemas.py
+++ comparalosproblemas.py
@@ -10,3 +10,29 @@ nota6 = randint(0,10)
 
 print("Las notas de Lucía son:\nClaridad del problema = {} \nOriginalidad = {} \nDificultad = {}".format(nota1,nota2,nota3))
 print("Las notas de Carlos son:\nClaridad del problema = {} \nOriginalidad = {} \nDificultad = {}".format(nota4,nota5,nota6))
+
+
+def comparar_Notas():
+    nota_total_Lucia = 0
+    nota_total_Carlos = 0
+    
+    if nota1 > nota4:
+        nota_total_Lucia += 1
+    elif nota1 < nota4:
+        nota_total_Carlos += 1
+    else:
+        pass
+    
+    if nota2 > nota5:
+        nota_total_Lucia += 1
+    elif nota2 < nota5:
+        nota_total_Carlos += 1
+    else:
+        pass
+    
+    if nota3 > nota6:
+        nota_total_Lucia += 1
+    elif nota3 < nota6:
+        nota_total_Carlos += 1
+    else:
+        pass
