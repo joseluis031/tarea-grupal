c9a9f7e04de360c29369e8b53750e2bfabe90f7a
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2021-12-16 13:25:39 +0100

    algo pasa con el bucle

--- juegodepiedras.py
+++ juegodepiedras.py
@@ -1,28 +1,29 @@
 import random
 
 piedras_totales = random.randint(10,25)
-print(piedras_totales)
+print("Hay",piedras_totales,"piedras")
 piedras_elegidas = 0
 
-def versus(piedras_totales,piedras_elegidas):
+def vs(piedras_totales,piedras_elegidas):
     if piedras_totales == 2:
         piedras_elegidas = 2
     elif piedras_totales == 3:
         piedras_elegidas = 3
     elif piedras_totales == 5:
         piedras_elegidas = 5
-    else:
+    elif piedras_totales != 2 or 3 or 5:
         while True:
             piedras_elegidas = random.randint(2,5)
             if piedras_elegidas == 4:
                 pass
     return piedras_elegidas
 
+
 turnos = random.randint(1,2)
-while True:
-    if turnos == 1:
+while piedras_totales > 1:
+    if turnos == 2:
         print("Turno del jugador 1")
-        piedras_elegidas = versus(piedras_totales, piedras_elegidas)
+        piedras_elegidas = vs(piedras_totales, piedras_elegidas)
         if piedras_elegidas == 2:
             piedras_totales -= 2
             print("Ha retirado 2 piedras")
@@ -34,11 +35,9 @@ while True:
         elif piedras_elegidas == 5:
             print("Ha retirado 5 piedras")
             print("Quedan:", piedras_totales, "piedras")
-        else:
-            pass
-    else:
+        break
+    elif turnos == 2:
         print("Turno del jugador 2")
-        piedras_elegidas = versus(piedras_totales, piedras_elegidas)
         if piedras_elegidas == 2:
             piedras_totales -= 2
             print("Ha retirado 2 piedras")
@@ -50,5 +49,4 @@ while True:
         elif piedras_elegidas == 5:
             print("Ha retirado 5 piedras")
             print("Quedan:", piedras_totales, "piedras")
-        else:
-            pass  
+        break
