27b4c85d392f808cc70616d3b2e2febd7fc3a7b7
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2021-12-16 16:33:41 +0100

    aplicando listas

--- comparalosproblemas.py
+++ comparalosproblemas.py
@@ -15,35 +15,36 @@ print("Las notas de Carlos son:\nClaridad del problema = {} \nOriginalidad = {} 
 def comparar_Notas():
     nota_total_Lucia = 0
     nota_total_Carlos = 0
-    
-    if nota1 > nota4:
+    a = [nota1,nota2,nota3]
+    b = [nota4,nota5,nota6]
+    if a[0] > b[0]:
         nota_total_Lucia += 1
-    elif nota1 < nota4:
+    elif a[0] < b[0]:
         nota_total_Carlos += 1
     else:
         pass
     
-    if nota2 > nota5:
+    if a[1] > b[1]:
         nota_total_Lucia += 1
-    elif nota2 < nota5:
+    elif a[1] < b[1]:
         nota_total_Carlos += 1
     else:
         pass
     
-    if nota3 > nota6:
+    if a[2] > b[2]:
         nota_total_Lucia += 1
-    elif nota3 < nota6:
+    elif a[2] < b[2]:
         nota_total_Carlos += 1
     else:
         pass
-    a = nota1 + nota2 + nota3
-    b = nota4 + nota5 + nota6
-    print("La nota total de lucía es:", a)
-    print("La nota total de Carlos es:", b)
+    c = nota1 + nota2 + nota3
+    d = nota4 + nota5 + nota6
+    print("La nota total de lucía es:", c)
+    print("La nota total de Carlos es:", d)
     
-    if a > b:
+    if c > d:
         print("Lucía tiene mas nota")
-    elif a < b:
+    elif c < d:
         print("Carlos tiene mas nota")
     else:
         print("Tienen la misma nota")
