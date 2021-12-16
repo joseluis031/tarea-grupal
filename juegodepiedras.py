0519229146866fbf21bd7622585d99f939c95442
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2021-12-16 12:13:44 +0100

    funcion para segun que numero de piedras salga

--- juegodepiedras.py
+++ juegodepiedras.py
@@ -1,6 +1,21 @@
 import random
 
 piedras_totales = random.randint(10,25)
+print(piedras_totales)
+piedras_elegidas = 0
 
-def versus():
-    
+def versus(piedras_totales,piedras_elegidas):
+    if piedras_totales == 2:
+        piedras_elegidas = 2
+    elif piedras_totales == 3:
+        piedras_elegidas = 3
+    elif piedras_totales == 5:
+        piedras_elegidas = 5
+    else:
+        while True:
+            piedras_elegidas = random.randint(2,5)
+            if piedras_elegidas == 4:
+                pass
+    return piedras_elegidas
+
+versus(piedras_totales,piedras_elegidas)
