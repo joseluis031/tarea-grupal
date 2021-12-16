deaff48c5a392abd61679c182e4b79ded1718a7b
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2021-12-16 14:04:08 +0100

    corregido

--- juegodepiedras.py
+++ juegodepiedras.py
@@ -6,22 +6,28 @@ piedras_elegidas = 0
 
 def vs(piedras_totales,piedras_elegidas):
     if piedras_totales == 2:
-        piedras_elegidas = 2
+        p = 2
     elif piedras_totales == 3:
-        piedras_elegidas = 3
+        p = 3
+    elif piedras_totales == 4:
+        p = 3
     elif piedras_totales == 5:
-        piedras_elegidas = 5
-    elif piedras_totales != 2 or 3 or 5:
+        p = 5
+    elif piedras_totales == 6:
+        p = 5
+    elif piedras_totales >=7:
         while True:
-            piedras_elegidas = random.randint(2,5)
-            if piedras_elegidas == 4:
+            p = random.randint(2,5)
+            if p == 4:
                 pass
-    return piedras_elegidas
+            else:
+                break
+    return p
 
 
 turnos = random.randint(1,2)
 while piedras_totales > 1:
-    if turnos == 2:
+    if turnos == 1:
         print("Turno del jugador 1")
         piedras_elegidas = vs(piedras_totales, piedras_elegidas)
         if piedras_elegidas == 2:
@@ -33,9 +39,10 @@ while piedras_totales > 1:
             print("Ha retirado 3 piedras")
             print("Quedan:", piedras_totales, "piedras")
         elif piedras_elegidas == 5:
+            piedras_totales -= 5
             print("Ha retirado 5 piedras")
             print("Quedan:", piedras_totales, "piedras")
-        break
+        
     elif turnos == 2:
         print("Turno del jugador 2")
         if piedras_elegidas == 2:
@@ -47,6 +54,7 @@ while piedras_totales > 1:
             print("Ha retirado 3 piedras")
             print("Quedan:", piedras_totales, "piedras")
         elif piedras_elegidas == 5:
+            piedras_totales -=5
             print("Ha retirado 5 piedras")
             print("Quedan:", piedras_totales, "piedras")
         break
