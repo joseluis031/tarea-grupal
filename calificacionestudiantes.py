a6224db4213e085a84facdcd0653ddb31e26f692
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2021-12-16 17:45:36 +0100

    he hecho esto...

--- calificacionestudiantes.py
+++ calificacionestudiantes.py
@@ -1,16 +1,53 @@
 notas = [73,67,38,33]
 
-multiplos_5_apartirde_40 = [45,50,55,60,65,70,75,80,85,90,95,100]
-
-if notas[0] < 37:
-    print("La calificación del estudiante 1 no se modificará, estás suspenso")
-if notas[1] < 37:
-    print("La calificación del estudiante 2 no se modificará, estás suspenso")
-if notas[2] < 37:
-    print("La calificación del estudiante 3 no se modificará, estás suspenso")
-if notas[3] < 37:
-    print("La calificación del estudiante 4 no se modificará, estás suspenso; su nota es:", notas[3])
+multiplos_5_apartirde_40 = [40,45,50,55,60,65,70,75,80,85,90,95,100]
 
 
-if notas > 37:
-    
+def notas_1estudiante():
+    if notas[0] < 40:
+        print("El estudiante 1 esta suspenso")
+    elif notas[0] > 40:
+        a = 75 - notas[0]
+        if a <= 3:
+            print("La nota de estudiante 1 se redondeará a 75")
+        
+notas_1estudiante()
+
+def notas_2estudiante():
+    if notas[1] < 40:
+        print("El estudiante 2 esta suspenso")
+    elif notas[1] > 40:
+        b = 70 - notas[1]
+        if b <= 3:
+            print("La nota de estudiante 2 se redondeará a 75")
+            
+notas_2estudiante()
+
+def notas_3estudiante():
+    if notas[2] < 40:
+        c = 40 - notas[2]
+        if c <= 3:
+            print("El estudiante 3 ha aprobado por redondeo")
+        elif c >= 3:
+            print("El estudiante 3 esta suspenso")
+    elif notas[2] > 40:
+        d = 40 - notas[2]
+        if d <= 3:
+            print("La nota de estudiante 3 se redondeará a 40")
+
+notas_3estudiante()
+
+def notas_4estudiante():
+    if notas[3] < 40:
+        e = 40 - notas[3]
+        if e <= 3:
+            print("El estudiante 3 ha aprobado por redondeo")
+        elif e >= 3:
+            print("El estudiante 4 esta suspenso")
+    elif notas[3] > 40:
+        f = 40 - notas[3]
+        if f <= 3:
+            print("La nota de estudiante 1 se redondeará a 40")
+            
+            
+notas_4estudiante()
