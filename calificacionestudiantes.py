41f53d6c7b6523481aef3c5a452ade98cb27e9b0
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2021-12-16 16:22:54 +0100

    commit

---
+++ calificacionestudiantes.py
@@ -0,0 +1,16 @@
+notas = [73,67,38,33]
+
+multiplos_5_apartirde_40 = [45,50,55,60,65,70,75,80,85,90,95,100]
+
+if notas[0] < 37:
+    print("La calificación del estudiante 1 no se modificará, estás suspenso")
+if notas[1] < 37:
+    print("La calificación del estudiante 2 no se modificará, estás suspenso")
+if notas[2] < 37:
+    print("La calificación del estudiante 3 no se modificará, estás suspenso")
+if notas[3] < 37:
+    print("La calificación del estudiante 4 no se modificará, estás suspenso; su nota es:", notas[3])
+
+
+if notas > 37:
+    
