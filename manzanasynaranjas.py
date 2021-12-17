3a638fb90d0b4bbec8b411f7a470f914a0fcfddc
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2021-12-17 17:41:17 +0100

    commit

--- manzanasynaranjas.py
+++ manzanasynaranjas.py
@@ -13,3 +13,20 @@ def contador(a, b, c, d, manzanas, naranjas):
     for naranja in naranjas:
         if(d+naranja >= a and d+naranja <= b):
             naranjas_en_caja +=1
+    print("Han caido " +str(manzanas_en_caja) + " manzanas dentro")
+    print("Han caido " + str(naranjas_en_caja) + " naranjas dentro")
+
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+a = int(first_multiple_input[0])
+b = int(first_multiple_input[1])
+second_multiple_input = input().rstrip().split()
+c = int(second_multiple_input[0])
+d = int(second_multiple_input[1])
+third_multiple_input = input().rstrip().split()
+m = int(third_multiple_input[0])
+n = int(third_multiple_input[1])
+
+manzanas = list(map(int, input().rstrip().split()))
+naranjas = list(map(int, input().rstrip().split()))
+contador(a, b, c, d, manzanas, naranjas)
