870c97d94aa9e6988dbb1e0813e0828d42c3aa01
Barroso03 <91721590+Barroso03@users.noreply.github.com>
2021-12-18 19:32:17 +0100

    Le decimos a la rana que busque el tunel para poder llegar al final del laberinto

--- ranalaberinto.py
+++ ranalaberinto.py
@@ -18,3 +18,16 @@ class Tunel:
     def __init__(self, x1, y1, x2, y2):
         self.extremo1 = Coordenadas(x1, y1)
         self.extremo2 = Coordenadas(x2, y2)
+# Le decimos a la rana que busque el tunel para poder llegar al final del laberinto
+def buscaTunel(Casillax,Casillay,tuneles):
+    coordenadas = Coordenadas(Casillax, Casillay)
+    for t in tuneles:
+        if(t.extremo1.comparate(Casillax,Casillay)==True):
+            coordenadas.x=t.extremo2.x
+            coordenadas.y=t.extremo2.y
+            break
+        elif(t.extremo2.comparate(Casillax,Casillay)==True):
+            coordenadas.x=t.extremo1.x
+            coordenadas.y=t.extremo1.y
+            break
+    return coordenadas
