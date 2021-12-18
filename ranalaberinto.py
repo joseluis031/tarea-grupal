7667f9dc8de0a25ce51cc8d2997e8c696d741cc3
Barroso03 <91721590+Barroso03@users.noreply.github.com>
2021-12-18 19:30:19 +0100

    definimos las coordenadas del laberinto

---
+++ ranalaberinto.py
@@ -0,0 +1,20 @@
+import math
+import os
+import random
+import re
+import sys
+# Definimos coordenadas
+class Coordenadas:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def comparate(self,x,y):
+        if(self.x==x and self.y==y):
+            return True
+        else:
+            return False
+# Definimos las coordenadas del tunel
+class Tunel:
+    def __init__(self, x1, y1, x2, y2):
+        self.extremo1 = Coordenadas(x1, y1)
+        self.extremo2 = Coordenadas(x2, y2)
